a697a1cc95ef224aff6ec06d188ee4f9eea365b4
hasbialwikusmana <hasbialwi70@gmail.com>
2022-06-23 20:27:34 +0700

    main route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Heading,
+  IconButton,
+  VStack,
+  useColorMode,
+  useToast,
+  Link,
+  Flex,
+} from "@chakra-ui/react";
+import TaskList from "./components/tasks";
+import AddTask from "./components/AddTask";
+import {
+  FaSun,
+  FaMoon,
+  FaGithub,
+  FaLinkedin,
+  FaInstagram,
+} from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 function App() {
+  const toast = useToast();
+  const [tasks, setTasks] = useState(
+    () => JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  function deleteTask(id) {
+    const newTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+    setTasks(newTasks);
+  }
+
+  function deleteTaskAll() {
+    setTasks([]);
+  }
+
+  function checkTask(id) {
+    const newTasksCheck = tasks.map((task, index, array) => {
+      if (task.id === id) {
+        task.check = !task.check;
+      }
+      return task;
+    });
+
+    setTasks(newTasksCheck);
+  }
+
+  function updateTask(id, body, onClose) {
+    const info = body.trim();
+
+    if (!info) {
+      toast({
+        title: "Input your task!",
+        position: "top",
+        status: "warning",
+        duration: 2000,
+        isClosable: true,
+      });
+
+      return;
+    }
+
+    const newTasksUpdate = tasks.map((task, index, array) => {
+      if (task.id === id) {
+        task.body = body;
+        task.check = false;
+      }
+      return task;
+    });
+
+    setTasks(newTasksUpdate);
+
+    onClose();
+  }
+
+  function addTask(task) {
+    setTasks([...tasks, task]);
+  }
+
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+    <VStack p={4} minH="100vh" pb={28}>
+      <IconButton
+        icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
+        isRound="true"
+        size="md"
+        alignSelf="flex-end"
+        onClick={toggleColorMode}
+      />
+
+      <Heading
+        p="5"
+        fontWeight="extrabold"
+        size="xl"
+        bgGradient="linear(to-l, teal.300, blue.500)"
+        bgClip="text"
+      >
+        Todo List
+      </Heading>
+      <AddTask addTask={addTask} />
+      <TaskList
+        tasks={tasks}
+        updateTask={updateTask}
+        deleteTask={deleteTask}
+        deleteTaskAll={deleteTaskAll}
+        checkTask={checkTask}
+      />
+
+      <Flex position="absolute" bottom="5">
+        <Link href="https://github.com/mrcodexH" target="_blank">
+          <IconButton icon={<FaGithub />} isRound="true" size="md" m="1" />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/hasbialwikusmana/"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <IconButton icon={<FaLinkedin />} isRound="true" size="md" m="1" />
+        </Link>
+        <Link
+          href="https://www.instagram.com/hasbialwikusmana/"
+          target="_blank"
+        >
+          <IconButton icon={<FaInstagram />} isRound="true" size="md" m="1" />
+        </Link>
+      </Flex>
+    </VStack>
   );
 }
 
